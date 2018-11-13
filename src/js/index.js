91a990f94cfaf242c57266a769b15d0f8e12b9e3
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-11-13 19:06:31 +0200

    testing api request

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,17 @@
-// Global app controller
+import axios from 'axios';
 
-import num from './test';
+async function getResult(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '199125905b110cd830a0145b7816c544';
+    try {
+        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        const count = res.data.count;
+        console.log(count);
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }  
+}
 
-console.log(`I imported ${num} from another module called test`);
+getResult('pizza');
