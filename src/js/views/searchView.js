2344bb198c86773dcf13fa719dae08f9de388395
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-11-16 20:57:26 +0200

    add method to limit recipe title

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -10,6 +10,22 @@ export const clearResult = () => {
     elements.searchResList.innerHTML = '';
 };
 
+const limitRecipeTitle = (title, limit = 25) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+
+        // return the result
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+}
+
 const renderRecipe = recipe => {
     const markup = `
     <li>
@@ -18,7 +34,7 @@ const renderRecipe = recipe => {
                 <img src="${recipe.image_url}" alt="${recipe.title}">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                 <p class="results__author">${recipe.publisher}</p>
             </div>
         </a>
