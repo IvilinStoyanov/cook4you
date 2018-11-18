70c2b89169dd7b3ef00d9eb296739c661a0ca39e
IvilinStoyanov <ivilin.stoyanov9@gmail.com>
2018-11-19 00:00:51 +0200

    import List class for testing purpose

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
@@ -114,3 +115,4 @@ elements.recipe.addEventListener('click', e => {
     }
 });
 
+window.l = new List();
