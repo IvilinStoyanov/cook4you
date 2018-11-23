55e1c61ab839a0f168955b8580e0f5d462cd7a61
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-11-23 17:14:24 +0200

    change the api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
  export const proxy = 'https://cors-anywhere.herokuapp.com/';
- export const key = '718d9b06afa3bf5ceb370e9e58830da0';
+ export const key = '199125905b110cd830a0145b7816c544';
  
  // 718d9b06afa3bf5ceb370e9e58830da0 - testing purpose 
 
