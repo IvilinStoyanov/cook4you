e4e8229d6853199c6415f7437f8aa084ad91a01c
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2019-01-06 19:58:07 +0200

    remove config.js file

--- src/js/config.js
+++
@@ -1,7 +0,0 @@
- export const proxy = 'https://cors-anywhere.herokuapp.com/';
- export const key = '199125905b110cd830a0145b7816c544';
- 
- // 718d9b06afa3bf5ceb370e9e58830da0 - testing purpose 
-
- // 199125905b110cd830a0145b7816c544 - i
-
