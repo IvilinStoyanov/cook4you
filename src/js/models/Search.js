b4f4a390938c3a3ac089249dc7eff96dd94522dd
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2020-06-22 16:41:27 +0300

    Update Search.js

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -10,7 +10,7 @@ export default class Search {
             const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
             this.result = res.data.recipes;
         } catch (error) {
-            alert(error);
+            alert("Couldn't find recipe with that name");
         }
     }
 }
